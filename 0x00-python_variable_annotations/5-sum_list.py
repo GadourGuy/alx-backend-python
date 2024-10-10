0ecb8c0d9ec4eb4c6747bb8ddd111a75534c3baa
Your Name <you@example.com>
2024-10-11 00:36:03 +0800

    adding task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''Task 5's module.
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+	''' a function that sums a list of floats
+	'''
+	return float(sum(input_list))
