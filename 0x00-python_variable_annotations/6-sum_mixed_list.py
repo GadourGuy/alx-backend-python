14e9509bc2df514e651d99a79b01307ea9cf48f0
Your Name <you@example.com>
2024-10-11 00:42:39 +0800

    fixing task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 '''Task 6's module.
 '''
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[float, int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     ''' sums a list
     '''
     return float(sum(mxd_lst))
