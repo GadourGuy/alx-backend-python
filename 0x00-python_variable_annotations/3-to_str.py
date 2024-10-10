8ed60f42a041b4050b84d09f34b2f8f91ffb543f
Your Name <you@example.com>
2024-10-11 00:28:26 +0800

    fixing task 3

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -4,6 +4,6 @@
 
 
 def to_str(n: float) -> str:
-	'''a type-annotated function that takes a float n as argument and returns the string representation of the float.
-	'''
-	return str(n)
+    '''takes a float n as argument and returns a string.
+    '''
+    return str(n)
