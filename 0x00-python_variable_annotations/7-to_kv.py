8d6f7376cfbf67a132caac6c87399634a4b3a04c
Your Name <you@example.com>
2024-10-11 00:53:17 +0800

    fixing task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 '''Task 7's module.
 '''
-from typing import List, Union
+from typing import Tuple, Union
 
-def to_kv(k: str, v: int | float) -> tuple[str, float]:
+
+def to_kv(k: str, v: int | float) -> Tuple[str, float]:
     '''Converts a key and its value to a tuple and a square
     '''
     return (k , float(v ** 2))
