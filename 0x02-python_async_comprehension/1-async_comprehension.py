9c9a2bbfc15128a464ea8945c22e5e2145d21f75
Your Name <you@example.com>
2024-10-15 17:52:49 +0800

    fixing task 1 issue

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,6 +7,7 @@ from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
-	'''Creates a list of 10 numbers from a 10-number generator.'''
-	return [i async for i in async_generator()]
+    '''Creates a list of 10 numbers from a 10-number generator.'''
+    return [i async for i in async_generator()]
