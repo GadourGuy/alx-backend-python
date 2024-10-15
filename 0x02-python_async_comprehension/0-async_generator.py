e47be2a4e033714a209426c15fca3bd790fdc94a
Your Name <you@example.com>
2024-10-15 17:14:44 +0800

    fixing task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,13 +2,14 @@
 '''Task 0's module.
 '''
 import asyncio
-from random import random
-from typing import AsyncGenerator
+import random
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     '''Generates a sequence of 10 numbers.
     '''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random() * 10
+        yield random.random() * 10
+        
